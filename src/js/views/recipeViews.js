3c2d636dba1b794ef011f994227783324bd1601d
mrlegend99 <mrahman.ruet16@gmail.com>
2023-05-28 00:07:28 +0600

    Fracty

--- src/js/views/recipeViews.js
+++ src/js/views/recipeViews.js
@@ -2,6 +2,7 @@ import { Fraction } from 'fractional';
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 import { updateServings } from '../model.js';
+import fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -127,7 +128,8 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      //ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? fracty(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
